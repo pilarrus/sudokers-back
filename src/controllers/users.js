6aefebd812bd288ddb0f735f4bd303f5c6944478
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-04-07 16:39:31 +0200

    Created user in the database

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,14 +1,28 @@
 const bcrypt = require('bcrypt');
+const Users = require('../mongo/models/users');
 
 const createUser = async (req, res) => {
   console.log('req.body', req.body);
 
   try {
-    const hash = await bcrypt.hash(req.body.password, 15);
-    console.log('FIN', hash);
+    const { username, password, email } = req.body;
+
+    const hash = await bcrypt.hash(password, 15);
+    // console.log('FIN', hash);
+
+    await Users.create({
+      username,
+      password: hash,
+      email
+    });
+
     res.send({status: 'OK', message: 'user created'});
 
   } catch (e) {
+    if(e.code && e.code === 11000) {
+      res.status(400).send({status: 'DUPLICATED_ VALUES', message: e.keyValue});
+      return;
+    }
     console.log(e);
     res.status(500).send({status: 'ERROR', message: e.message});
   }
